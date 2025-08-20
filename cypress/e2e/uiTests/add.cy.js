f09de49d254a7bfb1773e8e7ddcb8565be97f5a1
Maya Torres <sarytorres0703@gmail.com>
2025-08-20 10:19:47 -0600

    Delete credentials and replace for variables

--- cypress/e2e/uiTests/add.cy.js
+++ cypress/e2e/uiTests/add.cy.js
@@ -1,8 +1,8 @@
 describe('TC1 - Add Employee', () => {
   it('Should add an employe and validate if is displayed', () => {
-    cy.visit('https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login');
-    cy.get('input[id="Username"]').type('TestUser768');
-    cy.get('input[id="Password"]').type('|G!C_Ha6N)nz');
+    cy.visit(Cypress.env('LOGIN_URL'));
+    cy.get('input[id="Username"]').type(Cypress.env('USERNAME'));
+    cy.get('input[id="Password"]').type(Cypress.env('PASSWORD'));
     cy.get('button[type="submit"]').click();
     cy.contains('Paylocity Benefits Dashboard');
     cy.get('button[id="add"]').click();
